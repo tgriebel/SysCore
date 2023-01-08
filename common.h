3d66e61b8d7c961b1bf158425bedf9fe205265b7
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2023-01-08 10:19:09 -0600

    Start moving generic defines to SysCore

---
+++ common.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+const uint32_t KB_1 = 1024;
+const uint32_t MB_1 = 1024 * KB_1;
+const uint32_t GB_1 = 1024 * MB_1;
+
+#define KB( N ) ( N * KB_1 )
+#define MB( N ) ( N * MB_1 )
+#define GB( N ) ( N * GB_1 )
