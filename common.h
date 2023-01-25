d961dd14944abcc7b76980407b22d769197a30e3
Thomas Griebel <631199+tgriebel@users.noreply.github.com>
2023-01-25 09:13:11 -0600

    Add Count Array macro

--- common.h
+++ common.h
@@ -9,3 +9,4 @@ const uint32_t GB_1 = 1024 * MB_1;
 #define KB( N ) ( N * KB_1 )
 #define MB( N ) ( N * MB_1 )
 #define GB( N ) ( N * GB_1 )
+#define COUNTARRAY( ary ) static_cast<uint32_t>( sizeof( ary ) / sizeof( ary[0] ) )
